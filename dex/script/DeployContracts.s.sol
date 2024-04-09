e115197598280940d7264b8c6e81ae65cc4be07d
Nesquiko <lukas.castven@gmail.com>
2024-04-09 11:57:16 +0200

    fix slither issue, change solidity version to 0.8.18

--- dex/script/DeployContracts.s.sol
+++ dex/script/DeployContracts.s.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.25;
+pragma solidity ^0.8.18;
 
 import {Script} from "forge-std/Script.sol";
 import {DeployConfig} from "./DeployConfig.s.sol";
