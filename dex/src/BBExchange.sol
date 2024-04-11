25cdb05aa7867a9fbac0b0d5d95462801cee1e97
Jakub Jelínek <jakubkoje@gmail.com>
2024-04-11 15:32:37 +0200

    swapping dex

--- dex/src/BBExchange.sol
+++ dex/src/BBExchange.sol
@@ -77,6 +77,7 @@ contract BBExchange is Ownable {
         /**
          * TODO: Implement this function ******
          */
+
     }
 
     // Function removeLiquidity: Removes liquidity given the desired amount of ETH to remove.
@@ -102,18 +103,72 @@ contract BBExchange is Ownable {
 
     // Function swapTokensForETH: Swaps your token with ETH
     // You can change the inputs, or the scope of your function, as needed.
-    function swapTokensForETH(uint256 amountTokens, uint256 max_exchange_rate) external payable {
-        /**
-         * TODO: Implement this function ******
-         */
+    function swapTokensForETH(uint256 tokenAmount, uint256 max_exchange_rate) external payable {
+        require(tokenAmount > 0, "Need tokens to swap");
+        require(token.balanceOf(msg.sender) >= tokenAmount, "Not enough tokens");
+
+        uint256 ethAmount = getEthAmount(tokenAmount);
+        uint256 actualExchangeRate = (ethAmount * 1e18) / tokenAmount; // assuming 18 decimal places for ETH
+        require(actualExchangeRate <= max_exchange_rate, "Exchange rate too high");
+
+            // Transfer tokens from user to contract
+        token.transferFrom(msg.sender, address(this), tokenAmount);
+
+            // Update reserves and send ETH to user
+        token_reserves += tokenAmount;
+        eth_reserves -= ethAmount;
+
+            // Transfer ETH to user
+        payable(msg.sender).transfer(ethAmount);
     }
 
     // Function swapETHForTokens: Swaps ETH for your tokens
     // ETH is sent to contract as msg.value
     // You can change the inputs, or the scope of your function, as needed.
     function swapETHForTokens(uint256 max_exchange_rate) external payable {
-        /**
-         * TODO: Implement this function ******
-         */
+        uint256 ethAmount = msg.value;
+        require(ethAmount > 0, "Need ETH to swap");
+
+        uint256 tokenAmount = getTokenAmount(ethAmount);
+        uint256 actualExchangeRate = (tokenAmount * 1e18) / ethAmount; // assuming 18 decimal places for tokens
+        require(actualExchangeRate <= max_exchange_rate, "Exchange rate too high");
+
+        // Update reserves and send tokens to user
+        eth_reserves += ethAmount;
+        token_reserves -= tokenAmount;
+
+        // Transfer Tokens to user
+        token.transfer(msg.sender, tokenAmount);
+    }
+
+
+    function getEthAmount(uint256 tokenAmount) private view returns (uint256) {
+        require(tokenAmount > 0, "Token amount must be greater than zero");
+        require(token_reserves > 0 && eth_reserves > 0, "Invalid reserves");
+
+        // Calculate ETH received without the fee
+        uint256 ethReceived = (tokenAmount * eth_reserves) / (token_reserves + tokenAmount);
+
+        // Calculate the fee on the ETH amount
+        uint256 fee = (ethReceived * swap_fee_numerator) / swap_fee_denominator;
+        ethReceived -= fee;
+
+        require(ethReceived <= eth_reserves, "Not enough ETH in reserves");
+        return ethReceived;
+    }
+
+    function getTokenAmount(uint256 ethAmount) private view returns (uint256) {
+        require(ethAmount > 0, "ETH amount must be greater than zero");
+        require(token_reserves > 0 && eth_reserves > 0, "Invalid reserves");
+
+        // Calculate Tokens received without the fee
+        uint256 tokensReceived = (ethAmount * token_reserves) / (eth_reserves + ethAmount);
+
+        // Calculate the fee on the token amount
+        uint256 fee = (tokensReceived * swap_fee_numerator) / swap_fee_denominator;
+        tokensReceived -= fee;
+
+        require(tokensReceived <= token_reserves, "Not enough Tokens in reserves");
+        return tokensReceived;
     }
 }
